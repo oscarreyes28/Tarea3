2411e39980149c6af8b80855dae7cbe16fb14455
oscarreyes28 <oscarreyes2897@gmail.com>
2018-09-21 18:57:58 -0500

    Ultimo commit, programas terminados

--- Infijo_posfijo.c
+++ Infijo_posfijo.c
@@ -26,3 +26,91 @@ void apilar(char elem) {
 char desapilar() {
 	return (expre[top--]);
 }
+
+// Funcion que define la prioridad en nuestro conjunto de operaciones
+int priori(char elem) {
+	switch (elem) {
+		case '#':
+			return 0;
+	case '(':
+		return 1;
+	case '+':
+	case '-':
+		return 2;
+	case '*':
+	case '/':
+		return 3;
+	}
+}
+
+/*
+// Funcion que convierte de infijo a posfijo
+*/
+void infijo_posfijo(char *infijo, char *posfijo) {
+	char ch, elem;
+	int i = 0, k = 0;
+
+	espacios(infijo);
+	apilar('#');
+
+	while ((ch = infijo[i++]) != '\n') {
+		if (ch == '(')
+			apilar(ch);
+		else if (isalnum(ch))
+			posfijo[k++] = ch;
+		else if (ch == ')') {
+			while (expre[top] != '(')
+				posfijo[k++] = desapilar();
+			elem = desapilar();
+		} else {
+			while (priori(expre[top]) >= priori(ch))
+				posfijo[k++] = desapilar();
+			apilar(ch);
+		}
+	}
+
+	while (expre[top] != '#')
+	posfijo[k++] = desapilar();
+
+	posfijo[k] = 0;
+}
+
+// Funcion que evalua la operacion posfija
+
+int evaluar(char *posfijo) {
+	char ch;
+	int i = 0, op1, op2;
+	while((ch = posfijo[i++]) != 0) {
+		if(isdigit(ch))
+			apilar(ch-'0');
+		else {
+			op2 = desapilar();
+			op1 = desapilar();
+			switch(ch) {
+				case '+' : apilar(op1+op2);
+				break;
+				case '-' : apilar(op1-op2);
+				break;
+				case '*' : apilar(op1*op2);
+				break;
+				case '/' : apilar(op1/op2);
+				break;
+			}
+		}
+	}
+	return expre[top];
+}
+
+void main() {
+
+	char inf[50], pos[50];
+	printf("Ingrse su operacion:");
+	fgets(inf, 50, stdin);
+
+	infijo_posfijo(inf, pos);
+
+	printf("Expresion infija: %s" ,inf);
+	printf("Expresion posfija: %s \n" ,pos);
+	top = -1;
+printf("Resultado : %d", evaluar(pos));
+}
